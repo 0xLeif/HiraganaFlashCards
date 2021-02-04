0594952be55540a7f3b9e1368ccfe43dc1ce4870
0xLeif <zmeriksen@icloud.com>
2021-02-04 16:38:26 -0600

    Initial Commit

---
+++ HiraganaFlashCards/HiraganaFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiraganaFlashCardsApp.swift
+//  HiraganaFlashCards
+//
+//  Created by Zach Eriksen on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HiraganaFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
